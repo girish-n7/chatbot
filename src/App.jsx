6705417637278400248ae7cb5a45e3f91f42c413
Girish_N <ngirish1729@gmail.com>
2023-07-30 11:22:38 +0530

    component, style updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,11 @@
-import { useState } from "react";
 import Header from "./components/Header";
+import Chat from "./components/Chat";
 
 export default function App() {
-  const [message, setMessage] = useState("");
-  const [chats, setChats] = useState([]);
-  const [isTyping, setIsTyping] = useState(false);
-
-  function handleSubmit(event) {
-    return chat(event, message); //calls the chat function
-  }
-
-  function handleChange(event) {
-    return setMessage(event.target.value); //sets the message state to user typed message (on every keystroke)
-  }
-
-  const chat = async (event, message) => {
-    event.preventDefault(); //prevents site from resetting after submit
-
-    if (!message) return;
-    setIsTyping(true);
-
-    let messages = chats;
-    messages.push({ role: "user", content: message });
-    setChats(messages); //duplicate the chats array and push the new message with role: user
-
-    setMessage(""); //after pushing into the array, set messages array to ""
-
-    // call the backend service api
-
-    fetch("http://localhost:3000/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        chats,
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        messages.push(data.output); //push the new message i.e, data into the messages array
-        setChats(messages); //set the chats array to the above
-        setIsTyping(false);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
   return (
     <main>
       <Header />
-
-      <div className="chat--container">
-        {chats && chats.length
-          ? chats.map((chat, index) => (
-              <p
-                key={index}
-                className={`chat--message ${
-                  chat.role === "user" ? "user--input" : ""
-                }`}
-              >
-                {chat.content}
-              </p>
-            ))
-          : ""}
-      </div>
-
-      <div className={isTyping ? "" : "hide"}>
-        <p>
-          <i>{isTyping ? "Typing..." : ""}</i>
-        </p>
-      </div>
-
-      <form action="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="message"
-          value={message}
-          placeholder="Ask anything."
-          onChange={handleChange}
-        />
-      </form>
+      <Chat />
     </main>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
-import { useState } from "react";
-// import logo from "";
+import { useState, useEffect } from "react";
+import logoWhite from "../assets/logoWhite.svg";
+import logoBlack from "../assets/logoBlack.svg";
+import light from "../assets/lightMode.svg";
+import dark from "../assets/darkMode.svg";
 
 export default function Header() {
   const [darkTheme, setDarkTheme] = useState(true);
@@ -8,13 +11,40 @@ export default function Header() {
     setDarkTheme((prevState) => !prevState);
   }
 
+  const theme = document.querySelector(":root").style;
+
+  useEffect(() => {
+    theme.setProperty("--text--ai", darkTheme ? "#fdfeff" : "#060307");
+    theme.setProperty("--text--user", "#fdfeff");
+    theme.setProperty(
+      "--background",
+      darkTheme
+        ? "linear-gradient(#010205,#162f5b)"
+        : "linear-gradient(#fdfeff, #d4e1f8)"
+    );
+    theme.setProperty("--user--message", darkTheme ? "#3c61dd" : "#385bcc");
+    theme.setProperty(
+      "--assistant--message",
+      darkTheme ? "#020509" : "#fcfdfe"
+    );
+    theme.setProperty("--input--style", darkTheme ? "#040913" : "#e6e9f1");
+  });
+
   return (
     <div className="header--container">
       <div className="header--head">
-        {/* <img src={logo} className="header--logo"></img> */}
-        <p className="header--title">AI Chatbot</p>
+        <img
+          src={darkTheme ? logoWhite : logoBlack}
+          className="header--logo"
+        ></img>
+        <p className="header--title">BATMAN</p>
       </div>
-      <p onClick={handleClick}>{darkTheme ? "dark" : "light"}</p>
+      <img
+        src={darkTheme ? light : dark}
+        onClick={handleClick}
+        alt=""
+        className="icon--theme"
+      ></img>
     </div>
   );
 }
